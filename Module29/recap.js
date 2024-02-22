bd996176991291dcfffd1afdd223e1ad248ebeab
Farid <mdfaridhossenrehad@gmail.com>
2024-02-22 08:34:43 +0600

    Module 29.5 start

---
+++ Module29/recap.js
@@ -0,0 +1,24 @@
+/*
+1. How to use js with html
+2. Get element or elements from dom
+    2-1: get ElementById
+    2-2: getElementByClassName
+    2-3: querySelector
+    2-3: querySelectorAll
+
+3. dynamically add element to the dom : document.createElement
+    3-1: set value to the innerHTML
+    
+4. get and set value from/to a dom element
+    4-1: use innerText (h1 , p , h5 , div , span , strong)
+    4-2: use value (input, textArea)
+
+5. add event listener (event handler)
+    5-1: Click , focus , blur, onchange , keyup
+    5-2: addEventListener onclick="myHandler()"
+
+6. use function to reduce duplicate code 
+
+7. event delegate and event bubble
+
+*/
